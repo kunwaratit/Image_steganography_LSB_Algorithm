fab33c308b0aa30c1b9577e3e323d8255e401289
sagar nemkul <sagarnemkul7@gmail.com>
2023-07-14 21:19:53 +0545

    verification

--- SSFSS/project_SSFSS/project_SSFSS/urls.py
+++ SSFSS/project_SSFSS/project_SSFSS/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 # from django.conf.urls.static import static
-from django.conf.urls import url
+from django.urls import re_path
 from app import views
 
 from app.views import *
